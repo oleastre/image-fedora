5a941806773d695acd9a0fc530685be491993cc7
Hal Martin <hmartin@online.net>
2018-04-10 18:01:09 +0200

    Update to official Fedora 27 image
    Update dockerfile to source Fedora from the official Fedora 27 Docker repository
    as multiarch does not have a recent build of Fedora available.
    
    Signed-off-by: Hal Martin <hmartin@online.net>

---
+++ 27/env.mk
@@ -0,0 +1,8 @@
+IMAGE_NAME = fedora
+IMAGE_VERSION = 27
+IMAGE_TITLE = Fedora 27
+IMAGE_DESCRIPTION = Fedora 27
+IMAGE_SOURCE_URL = https://github.com/scaleway/image-fedora
+IMAGE_VENDOR_URL = https://www.getfedora.org
+IMAGE_BOOTSCRIPT = mainline 4.4
+IMAGE_BASE_FLAVORS = common docker-based systemd feature-motd
