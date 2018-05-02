7c852494185ffa8b01ff31e0255af65ca0301e39
Hal Martin <hmartin@online.net>
2018-05-02 13:36:18 +0200

    Networking fixes for Fedora 28
    Signed-off-by: Hal Martin <hmartin@online.net>

---
+++ 28/env.mk
@@ -0,0 +1,8 @@
+IMAGE_NAME = fedora
+IMAGE_VERSION = 28
+IMAGE_TITLE = Fedora 28
+IMAGE_DESCRIPTION = Fedora 28
+IMAGE_SOURCE_URL = https://github.com/scaleway/image-fedora
+IMAGE_VENDOR_URL = https://www.getfedora.org
+IMAGE_BOOTSCRIPT = mainline 4.4
+IMAGE_BASE_FLAVORS = common docker-based systemd feature-motd
