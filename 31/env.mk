b20fb12a1ff788741bbbde4755ac2288d8e24cde
Olivier Samyn 🎻 <code@oleastre.be>
2019-11-15 10:35:40 +0100

    Initial arm 64 Fedora 31 support

---
+++ 31/env.mk
@@ -0,0 +1,8 @@
+IMAGE_NAME = fedora
+IMAGE_VERSION = 31
+IMAGE_TITLE = Fedora 31
+IMAGE_DESCRIPTION = Fedora 31
+IMAGE_SOURCE_URL = https://github.com/scaleway/image-fedora
+IMAGE_VENDOR_URL = https://www.getfedora.org
+IMAGE_BOOTSCRIPT = mainline 5.1.12 rev1
+IMAGE_BASE_FLAVORS = common docker-based systemd feature-motd
